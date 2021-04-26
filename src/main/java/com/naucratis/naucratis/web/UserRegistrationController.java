4e314136690cc745a14cbb49edd3eff3b7978fbd
jdcasallasm <jdcasallasm@unal.edu.co>
2021-04-26 09:26:06 -0500

    login frontend

--- src/main/java/com/naucratis/naucratis/web/UserRegistrationController.java
+++ src/main/java/com/naucratis/naucratis/web/UserRegistrationController.java
@@ -1,7 +1,8 @@
-package com.naucratis.naucratis.web.dto;
+package com.naucratis.naucratis.web;
 
 
 import com.naucratis.naucratis.service.UserService;
+import com.naucratis.naucratis.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
