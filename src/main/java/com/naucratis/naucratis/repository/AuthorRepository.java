783512d15c9d58c0c4c3a3cd8f3acd1460ad3371
crubioa <55598627+car1803@users.noreply.github.com>
2021-06-15 11:52:59 -0500

    Delete src directory

--- src/main/java/com/naucratis/naucratis/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.naucratis.naucratis.repository;
-
-import com.naucratis.naucratis.model.library.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, String>
-{
-
-}
