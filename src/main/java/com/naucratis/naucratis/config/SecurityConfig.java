808ff2a26ef75ec5a7d32a910ab74cfa923176b7
Julián Casallas <jdcasallasm@unal.edu.co>
2021-06-15 03:48:19 -0500

    Adición de restricciones de seguridad para la sección de librerías

--- src/main/java/com/naucratis/naucratis/config/SecurityConfig.java
+++ src/main/java/com/naucratis/naucratis/config/SecurityConfig.java
@@ -35,14 +35,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(
+        http.authorizeRequests()
+                .antMatchers("/library/**").hasRole("ADMINISTRATOR")
+                .antMatchers(
                 "/register**",
                 "/register/customer**",
                 "/register/administrator**",
                 "/js/**",
                 "/css/**",
                 "/img/**"
-        ).permitAll().anyRequest().authenticated()
+        ).permitAll()
+                .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
                 .and().logout().invalidateHttpSession(true).clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout").permitAll();
